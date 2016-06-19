d18c22d7855ff3e8b0210b8ed3be1648e794acaf
amankochhar <amanskochhar@gmail.com>
2016-06-19 10:20:42 -0400

    final

--- run_analysis.R
+++ run_analysis.R
@@ -72,8 +72,8 @@ data <- rbind(train, test)
 data <- arrange(data, id)
 
 #2) Extracts only the measurements on the mean and standard deviation for each measurement. 
-meanStd <- data[,c(1,2,grep("std", colnames(data)), grep("mean", colnames(data)))]
-saveResults(meanStd,"meanStd")
+mean_and_std <- data[,c(1,2,grep("std", colnames(data)), grep("mean", colnames(data)))]
+saveResults(mean_and_std,"mean_and_std")
 
 #3) Uses descriptive activity names to name the activities in the data set
 activity_labels <- getTables("activity_labels.txt")
@@ -82,6 +82,6 @@ activity_labels <- getTables("activity_labels.txt")
 data$activity <- factor(data$activity, levels=activity_labels$V1, labels=activity_labels$V2)
 
 #5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-tidyDataset <- ddply(meanStd, .(id, activity), .fun=function(x){ colMeans(x[,-c(1:2)]) })
-colnames(tidyDataset)[-c(1:2)] <- paste(colnames(tidyDataset)[-c(1:2)], "_mean", sep="")
-saveResults(tidyDataset,"tidyDataset")
+tidy_dataset <- ddply(mean_and_std, .(id, activity), .fun=function(x){ colMeans(x[,-c(1:2)]) })
+colnames(tidy_dataset)[-c(1:2)] <- paste(colnames(tidy_dataset)[-c(1:2)], "_mean", sep="")
+saveresults(tidy_dataset,"tidy_dataset")
